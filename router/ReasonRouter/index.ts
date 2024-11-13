2e90d9db839fee2f377d79981966609242dd497e
Vikaskumar8085 <vikaskumar7049002404@gmail.com>
2024-11-13 19:28:35 +0530

    update code

---
+++ router/ReasonRouter/index.ts
@@ -0,0 +1,7 @@
+import express from "express";
+import ReasonCtr from "../../controller/ReasonController/ReasonCtr";
+
+const ReasonRouter = express.Router();
+
+ReasonRouter.post("/create-reason", ReasonCtr.createReasonctr);
+export default ReasonRouter;
--- router/index.ts
+++ router/index.ts
@@ -1,12 +1,11 @@
 import express from "express";
 import userRouter from "./UserRouter";
-import candidateRouter from "./CandidateRouter"
+import candidateRouter from "./CandidateRouter";
 import designationRouter from "./DesignationRouter";
-
+import ReasonRouter from "./ReasonRouter";
 const indexRouter = express.Router();
-
 indexRouter.use("/user", userRouter);
 indexRouter.use("/designation", designationRouter);
 indexRouter.use("/candidate", candidateRouter);
-
+indexRouter.use("/reason", ReasonRouter);
 export default indexRouter;
