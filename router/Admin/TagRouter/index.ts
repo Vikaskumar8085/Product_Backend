56be73663ec9a82b725af6f47867ef6a4db824ca
ayush <ayush@ayush.ayush>
2024-11-19 13:29:05 +0530

    education crud

--- router/Admin/TagRouter/index.ts
+++ router/Admin/TagRouter/index.ts
@@ -1,10 +1,12 @@
 import {Router} from "express";
 import TagCtr from "../../../controller/TagsController/TagCtr";
-
+import upload from "../../../middleware/upload";
 const tagRouter = Router();
 
 // tag Router
 tagRouter.post("/create-tag", TagCtr.createtagCtr);
 tagRouter.get("/fetch-tag", TagCtr.fetchtagsCtr);
+tagRouter.post("/import-tags",upload.single("file"), TagCtr.importTagsCtr);
+tagRouter.get("/download-tag-template", TagCtr.returntagsCsvFile);
 
 export default tagRouter;
